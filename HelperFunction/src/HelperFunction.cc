a04c373540b5087aba047f31d109884ae6329a8e
tocheng <tocheng.cms@gmail.com>
2016-02-26 12:52:20 +0100

    update muon pterr to userFloat

--- HelperFunction/src/HelperFunction.cc
+++ HelperFunction/src/HelperFunction.cc
@@ -37,6 +37,7 @@ HelperFunction::HelperFunction()
         //declarations
         debug_ = 0;
 
+/*
         TString fmu_s = TString(edm::FileInPath ( "KinZfitter/HelperFunction/hists/ebeOverallCorrections.Legacy2013.v0.root" ).fullPath());
         TString fel_s = TString(edm::FileInPath ( "KinZfitter/HelperFunction/hists/ebeOverallCorrections.Legacy2013.v0.root" ).fullPath());
 
@@ -48,7 +49,7 @@ HelperFunction::HelperFunction()
                 
         electron_corr_data = boost::shared_ptr<TH2F>(  (static_cast<TH2F*>(fel->Get( "el_reco53x" )->Clone() )) );
         electron_corr_mc = boost::shared_ptr<TH2F>(  (static_cast<TH2F*>(fel->Get( "el_mc53x" )->Clone() )) );
-
+*/
 
 }
 
@@ -148,6 +149,8 @@ double HelperFunction::pterr( reco::Candidate *c, bool isData){
   reco::GsfElectron *gsf; reco::Muon *mu;
   reco::PFCandidate *pf;
 
+  pat::Muon *patmu;
+
   double pterrLep = 0.0;
 
   if ((gsf = dynamic_cast<reco::GsfElectron *> (&(*c)) ) != 0)
@@ -157,6 +160,17 @@ double HelperFunction::pterr( reco::Candidate *c, bool isData){
   else if ((mu = dynamic_cast<reco::Muon *> (&(*c)) ) != 0)
   {
     pterrLep=pterr(mu, isData);
+    if(debug_)cout<<"reco pt err is "<<pterrLep<<endl;
+
+    if( (patmu = dynamic_cast<pat::Muon *> (&(*c)) )!=0){
+
+     if ( patmu->hasUserFloat("correctedPtError") == true ) {
+       if(debug_) cout<<"use userFloat for muon pt err"<<endl;
+       pterrLep = patmu->userFloat("correctedPtError");
+       if(debug_) cout<<"calib pt err is "<<pterrLep<<endl;
+     }
+ 
+    }
   }
   else if ((pf = dynamic_cast<reco::PFCandidate *> (&(*c)) ) != 0)
   { 
@@ -174,26 +188,6 @@ double HelperFunction::pterr( reco::Muon* mu, bool isData){
 
         return pterr;
 }
-
-/*
-double HelperFunction::pterr( pat::Muon muon, bool isData){
-
-        TH2F* mu_corr;
-        if(isData) mu_corr = dynamic_cast<TH2F*> (muon_corr_data->Clone());
-        else mu_corr = dynamic_cast<TH2F*> (muon_corr_mc->Clone());
-        
-        TAxis* x_mupTaxis = mu_corr->GetXaxis(); TAxis* y_muetaaxis = mu_corr->GetYaxis();
-        double maxPt = x_mupTaxis->GetXmax(); double minPt = x_mupTaxis->GetXmin();
-                
-        const pat::Muon *mu = &muon;
-        double scaleFactormu = 1.0;
-        int xbin = x_mupTaxis->FindBin(mu->pt()); int ybin = y_muetaaxis->FindBin(fabs(mu->eta()));
-        if(mu->pt()>minPt && mu->pt()<maxPt){  scaleFactormu = mu_corr->GetBinContent(xbin,ybin);  }
-        double pterr = mu->muonBestTrack()->ptError() * scaleFactormu;
-
-        return pterr;
-}
-*/
 
 double HelperFunction::pterr( reco::GsfElectron * elec, bool isData ){
 
@@ -229,54 +223,6 @@ double HelperFunction::pterr( reco::GsfElectron * elec, bool isData ){
 
 }
 
-/*
-double HelperFunction::pterr( pat::Electron electron, bool isData ){
-
-        TH2F* el_corr;
-        if(isData) el_corr = dynamic_cast<TH2F*>(electron_corr_data->Clone());
-        else el_corr = dynamic_cast<TH2F*>(electron_corr_mc->Clone());
-        TAxis* x_elpTaxis = el_corr->GetXaxis(); TAxis* y_eletaaxis = el_corr->GetYaxis();
-        double maxPt = x_elpTaxis->GetXmax(); double minPt = x_elpTaxis->GetXmin();
-
-        const pat::Electron *elec = &(electron);
-        double perr = 0.;
-        if (elec->ecalDriven()) {
-
-              perr = elec->p4Error(reco::GsfElectron::P4_COMBINATION);
-
-        }
-        else {
-                 // Parametrization from Claude Charlot, 
-                 // http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/CJLST/ZZAnalysis/AnalysisStep/src/ZZMassErrors.cc?revision=1.2&view=markup
-#if CMSSW_VERSION<500
-                        double ecalEnergy = elec->ecalEnergy() ;
-#else
-                        double ecalEnergy = elec->correctedEcalEnergy() ;
-#endif
-                        double err2 = 0.0;
-                        if (elec->isEB()) {
-                                err2 += (5.24e-02*5.24e-02)/ecalEnergy;
-                                err2 += (2.01e-01*2.01e-01)/(ecalEnergy*ecalEnergy);
-                                err2 += 1.00e-02*1.00e-02;
-                        } else if (elec->isEE()) {
-                                err2 += (1.46e-01*1.46e-01)/ecalEnergy;
-                                err2 += (9.21e-01*9.21e-01)/(ecalEnergy*ecalEnergy);
-                                err2 += 1.94e-03*1.94e-03;
-                        }
-                        perr = ecalEnergy * sqrt(err2);
-         }
-
-         double scaleFactor_el = 1.0;
-
-         int xbin = x_elpTaxis->FindBin(elec->pt()); int ybin = y_eletaaxis->FindBin(fabs(elec->eta()));
-         if(elec->pt()>minPt && elec->pt()<maxPt ){  scaleFactor_el = el_corr->GetBinContent(xbin,ybin);  }
-
-         double pterr = scaleFactor_el*(perr*elec->pt()/elec->p());
-
-         return pterr;
-
-}
-*/
 
 double HelperFunction::pterr(TLorentzVector ph){
 
